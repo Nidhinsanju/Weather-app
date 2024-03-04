86f62d0be848565824b1985a1669c51d07c3e606
nidhin sanju <nidhinsanjuk@gmail.com>
2024-03-04 17:59:36 +0530

    Added units

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -32,101 +32,152 @@ export default function Bottombox() {
   const visibility = useRecoilValue(dataVisibility);
   const celius = temperture - 273.15;
   const decimal = parseFloat(celius).toFixed(2);
+  const date = new Date(sunSet * 1000);
+  const date1 = new Date(sunRise * 1000);
+  const hours1 = date1.getHours().toString().padStart(2, "0");
+  const minutes1 = date1.getMinutes().toString().padStart(2, "0");
+  const sunRiseime = `${hours1}:${minutes1}`;
+  const hours = date.getHours().toString().padStart(2, "0");
+  const minutes = date.getMinutes().toString().padStart(2, "0");
+  const sunSetime = `${hours}:${minutes}`;
 
   return (
     <article>
       <div className="mt-10 m-5 p-10 h-full bg-white flex flex-wrap items-center justify-center  ">
         <Carousel
-          className="w-full max-w-sm"
+          className="w-full max-w-sm "
           opts={{
             align: "start",
           }}
         >
           <CarouselContent>
-            <CarouselItem className="md:basis-1/2 lg:basis-1/2 m-5">
-              <Card className="p-28">
+            <CarouselItem className="md:basis-2/3 lg:basis-4/4 m-5  ">
+              <Card className="p-2">
                 <CardContent className="flex items-center justify-center p-2">
                   <div>
                     <h1 className="font-semibold ">Temperture</h1>
+                    <img
+                      className="w-full  min-h-52 max-h-52 object-fill"
+                      alt="Temperture Image"
+                      src="https://previews.123rf.com/images/igoun/igoun2003/igoun200300143/142778011-thermometer-icon-in-trendy-flat-design-simple-sign-of-temperature-measuring-weather-indicator.jpg"
+                    />
                     <span className="text-sm font-semibold">
-                      {temperture ? <h1>{temperture}°C </h1> : <h1>0°C</h1>}
-                    </span>
-                  </div>
-                </CardContent>
-              </Card>
-            </CarouselItem>{" "}
-            <CarouselItem className="md:basis-1/2 lg:basis-1/2 m-5">
-              <Card className="p-28">
-                <CardContent className="flex items-center justify-center p-2">
-                  <div>
-                    <span className="text-sm font-semibold ">Pressure</span>
-                    <span className="text-sm font-semibold">
-                      {" "}
-                      {pressure ? <h4>{pressure}°C </h4> : <h4>0°C</h4>}
+                      {decimal ? <h1>{decimal}°C </h1> : <h1>0°C</h1>}
                     </span>
                   </div>
                 </CardContent>
               </Card>
             </CarouselItem>
-            <CarouselItem className="md:basis-1/2 lg:basis-1/2 m-5">
-              <Card className="p-28">
+            <CarouselItem className="md:basis-2/3 lg:basis-4/4 m-5  ">
+              <Card className="p-2">
                 <CardContent className="flex items-center justify-center p-2">
                   <div>
-                    <span className="text-sm font-semibold ">Visibility</span>
+                    <h1 className="text-sm font-semibold ">Pressure</h1>
+                    <img
+                      className="w-full  min-h-52 max-h-52 object-fill"
+                      alt="Temperture Image"
+                      src="https://www.shutterstock.com/image-vector/atmospheric-pressure-line-icon-outline-260nw-716113417.jpg"
+                    />
                     <span className="text-sm font-semibold">
                       {" "}
-                      {visibility ? <h4>{visibility}°C </h4> : <h4>0°C</h4>}
+                      {pressure ? <h4>{pressure} (mb) </h4> : <h4> (mb),</h4>}
                     </span>
                   </div>
                 </CardContent>
               </Card>
             </CarouselItem>
-            <CarouselItem className="md:basis-1/2 lg:basis-1/2 m-5">
-              <Card className="p-28">
+            <CarouselItem className="md:basis-2/3 lg:basis-4/4 m-5  ">
+              <Card className="p-2">
                 <CardContent className="flex items-center justify-center p-2">
                   <div>
-                    <span className="text-sm font-semibold ">Sunrise</span>
+                    <h1 className="text-sm font-semibold ">Visibility</h1>
+                    <img
+                      className="w-full  min-h-52 max-h-52 object-fill"
+                      alt="Temperture Image"
+                      src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1PWiVuuxEJzhTw_kCr8nf3R3ASpf6EhnOV1XkYEWS0TKL7aWCNO4M4aa4Y0KfzuCypyU&usqp=CAU"
+                    />
                     <span className="text-sm font-semibold">
                       {" "}
-                      {sunRise ? <h4>{sunRise}°C </h4> : <h4>0°C</h4>}
+                      {visibility ? <h4>{visibility}(m) </h4> : <h4>(m)</h4>}
                     </span>
                   </div>
                 </CardContent>
               </Card>
             </CarouselItem>
-            <CarouselItem className="md:basis-1/2 lg:basis-1/2 m-5">
-              <Card className="p-28">
+            <CarouselItem className="md:basis-2/3 lg:basis-4/4 m-5  ">
+              <Card className="p-2">
                 <CardContent className="flex items-center justify-center p-2">
                   <div>
-                    <span className="text-sm font-semibold ">Sunset</span>
+                    <h1 className="text-sm font-semibold ">Sunrise</h1>
+                    <img
+                      className="w-full  min-h-52 max-h-52 object-fill"
+                      alt="Temperture Image"
+                      src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRuL3chZRpIFuaZvF7w9-WaxxqVfbYndfP8GVnuVPVJBcFmr9VCHgoax2lMC5vFA4aMBGQ&usqp=CAU"
+                    />
                     <span className="text-sm font-semibold">
                       {" "}
-                      {sunSet ? <h4>{sunSet}°C </h4> : <h4>0°C</h4>}
+                      {sunRiseime ? <h4>{sunRiseime}⏰ </h4> : <h4>⏰ </h4>}
                     </span>
                   </div>
                 </CardContent>
               </Card>
             </CarouselItem>
-            <CarouselItem className="md:basis-1/2 lg:basis-1/2 m-5">
-              <Card className="p-28">
+            <CarouselItem className="md:basis-2/3 lg:basis-4/4 m-5  ">
+              <Card className="p-2">
                 <CardContent className="flex items-center justify-center p-2">
                   <div>
-                    <span className="text-sm font-semibold ">Humidity</span>
+                    <h1 className="text-sm font-semibold ">Sunset</h1>
+                    <img
+                      className="w-full  min-h-52 max-h-52 object-fill"
+                      alt="Temperture Image"
+                      src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQBVEX1RTj-OAayTN2GJLTkxpqU6IuxaEiXz1CTQfYLWVm3SMrvoUPyUN-_KlJiQxGKvec&usqp=CAU"
+                    />
                     <span className="text-sm font-semibold">
-                      {Humidity ? <h4>{Humidity}°C </h4> : <h4>0°C</h4>}
+                      {" "}
+                      {sunSetime ? <h4>{sunSetime}⏲ </h4> : <h4>⏲</h4>}
                     </span>
                   </div>
                 </CardContent>
               </Card>
             </CarouselItem>
-            <CarouselItem className="md:basis-1/2 lg:basis-1/2 m-5">
-              <Card className="p-28">
+            <CarouselItem className="md:basis-2/3 lg:basis-4/4 m-5  ">
+              <Card className="p-2">
                 <CardContent className="flex items-center justify-center p-2">
                   <div>
-                    <span className="text-sm font-semibold ">Windspeed</span>
+                    <h1 className="text-sm font-semibold ">Humidity</h1>
+                    <img
+                      className="w-full  min-h-52 max-h-52 object-fill"
+                      alt="Temperture Image"
+                      src="https://cdn.vectorstock.com/i/preview-1x/76/62/humidity-percentage-icon-vector-39587662.jpg"
+                    />
+                    <span className="text-sm font-semibold">
+                      {Humidity ? (
+                        <h4>{Humidity}( g.m-3) </h4>
+                      ) : (
+                        <h4>0 ( g.m-3)</h4>
+                      )}
+                    </span>
+                  </div>
+                </CardContent>
+              </Card>
+            </CarouselItem>
+            <CarouselItem className="md:basis-2/3 lg:basis-4/4 m-5  ">
+              <Card className="p-2">
+                <CardContent className="flex items-center justify-center p-2">
+                  <div>
+                    <h1 className="text-sm font-semibold ">Windspeed</h1>
+                    <img
+                      className="w-full  min-h-52 max-h-52 object-fit"
+                      alt="Temperture Image"
+                      src="https://media.istockphoto.com/id/1222702106/vector/weather-forecast-line-icon-of-wind-isolated-on-white-background-weather-symbol-in-modern.jpg?s=170667a&w=0&k=20&c=6o3S4ndbVDlXwrEjWz50spkXm0m3JPBth1Xmlz_nBAg="
+                    />
                     <span className="text-sm font-semibold">
                       {" "}
-                      {windSpeed ? <h4>{windSpeed}°C </h4> : <h4>0°C</h4>}
+                      {windSpeed ? (
+                        <h4>{windSpeed}(km/h) </h4>
+                      ) : (
+                        <h4>0(km/h)</h4>
+                      )}
                     </span>
                   </div>
                 </CardContent>
