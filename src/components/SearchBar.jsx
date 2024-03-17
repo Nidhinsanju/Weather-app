faae234ee12be9963a34012a2620065006bb3f26
nidhin sanju <nidhinsanjuk@gmail.com>
2024-03-17 15:44:46 +0530

    changes

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -53,7 +53,7 @@ export default function Search(pros) {
           <input
             type="text"
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder="Search branch name..."
+            placeholder="Search City "
             onChange={(e) => {
               setCity(e.target.value);
             }}
